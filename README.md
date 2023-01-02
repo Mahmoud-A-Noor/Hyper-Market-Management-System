4d2cf8c96771f46d6e5a706e22c970ad37d71ee1
MahmoudNoor <mahmoudnoor917@gmail.com>
2023-01-02 11:14:54 +0200

    Hyper Market Management System

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# Hyper-Market-Management-System
+# Hyper Market Management System
+
+## Requirements
+
+- JDK
+- Xampp
+
+## Usage
+
+1. Setup JDK 14.0.2 (other versions may also work)
+2. Setup Xampp
+    1. Run Apache server
+    2. Run mySQL server
+    3. Open mySQL admin page (phpMyAdmin)
+        1. Create database with name **market**
+        2. Import the database file **market.sql** provided in the dependencies folder
+
+## Modules
+
+every module has it's own default username and password
+
+![login.PNG](dependencies/previews/login.png)
+
+### Administration Module
+
+**username:** admin <br>
+**password:** admin
+
+- Admin has username and password and can alter them.
+- Admin manages employees (Add - set unique id, password and type to new employee, Delete, update, List and search all Employees, ).
+
+![admin.PNG](dependencies/previews/admin.png)
+
+### Marketing Module
+
+**username:** marketing <br>
+**password:** marketing
+
+- Marketing employees can make reports about products (make queries).
+- Marketing employees make special offers and send them to inventory management.
+
+![marketing.PNG](dependencies/previews/marketing.png)
+
+### Inventory Module
+
+**username:** inventory <br>
+**password:** inventory
+
+- Inventory employees (Add, Delete, Update, List, Search) Products.
+- System Must send notification when amount of product reduces at special range -set by inventory employees- OR when expiry date of product gets close.
+- Manage the Damages items and sales Return.
+
+![inventory.PNG](dependencies/previews/inventory.png)
+
+### Sales Module
+
+**username:** sales <br>
+**password:** sales
+
+- Seller can (search for product, list all products, make and cancel order or cancel part of the order).
+
+![seller.PNG](dependencies/previews/seller.png)
+
+![seller-previous.PNG](dependencies/previews/seller-previous.png)
---
+++ dependencies/market.sql
@@ -0,0 +1,115 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: market
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idemployees_UNIQUE` (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'admin','admin','Admin'), (2,'inventory','inventory','Inventory Employee'),(3,'sales','sales','Seller'),(4,'marketing','marketing','Marketing Employee');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `Order_ID` int NOT NULL AUTO_INCREMENT,
+  `Product_name` varchar(45) NOT NULL,
+  `Product_category` varchar(45) NOT NULL,
+  `Quantity` int NOT NULL,
+  `Bill` int NOT NULL,
+  `Date` date NOT NULL,
+  PRIMARY KEY (`Order_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock`
+--
+
+DROP TABLE IF EXISTS `stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stock` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `category` varchar(45) NOT NULL,
+  `production_date` date NOT NULL,
+  `expiry_date` date NOT NULL,
+  `price` double NOT NULL,
+  `quantity` int DEFAULT NULL,
+  `damages` int DEFAULT '0',
+  `shortage` int DEFAULT '0',
+  `offer` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock`
+--
+
+LOCK TABLES `stock` WRITE;
+/*!40000 ALTER TABLE `stock` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stock` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-01 23:08:48
